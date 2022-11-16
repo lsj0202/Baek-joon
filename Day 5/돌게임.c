e65e3f848096857e17ffb4f2bfe3d64710afc941
lsj0202 <w1004123@gmail.com>
2022-11-16 22:29:32 +0900

    Day 5

---
+++ Day 5/돌게임.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    
+    if (n % 2 == 1) {
+		printf("SK");
+	}
+	else {
+		printf("CY");
+	}
+    
+    return 0;
+}
+
+//파이썬으로 안풀고 C로 풀어버림
